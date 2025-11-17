29293e0889acae0c9e2828e826305544f73e7ff6
joe052 <joekaranjasenior52@gmail.com>
2025-11-17 16:11:49 +0300

    added login and signup pages

---
+++ productive-app/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignUpForm from "@/components/auth/SignUpForm";
+
+/**COMPONENT */
+const SignUpPage: React.FC = () => {
+  /**TEMPLATE */
+  return <SignUpForm />;
+};
+
+export default SignUpPage;
