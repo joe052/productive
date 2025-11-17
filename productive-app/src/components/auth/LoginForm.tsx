0a6cad5c1cb7c76369e9a56f0eccc613cd23685b
joe052 <joekaranjasenior52@gmail.com>
2025-11-17 14:52:13 +0300

    message

---
+++ productive-app/src/components/auth/LoginForm.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+/**COMPONENT */
+const LoginForm: React.FC = () => {
+  /**VARIABLES */
+
+  /**FUNCTIONS */
+
+  /**TEMPLATE */
+  return <div className="">Login Form</div>;
+};
+
+export default LoginForm;
