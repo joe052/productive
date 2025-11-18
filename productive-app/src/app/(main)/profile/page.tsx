2400b3a7fcd660d32439a26460ad73cf98f4e095
joe052 <joekaranjasenior52@gmail.com>
2025-11-18 12:25:41 +0300

    created profile route

---
+++ productive-app/src/app/(main)/profile/page.tsx
@@ -0,0 +1,14 @@
+import ProfileComponent from "@/components/profile/ProfileComponent";
+import React from "react";
+
+/**COMPONENT */
+const ProfilePage: React.FC = () => {
+  /**VARIABLES */
+
+  /**FUNCTIONS */
+
+  /**TEMPLATE */
+  return <ProfileComponent />;
+};
+
+export default ProfilePage;
