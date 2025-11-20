b67194a114231bef4c5f1d52bbb4fe625048fd67
Obed Kiplimo <obed.kiplimo@vault.lishailabs.com>
2025-11-20 08:40:20 +0300

    adjusted the colour of the add task button

--- productive-app/src/components/ui/AddTaskButton.tsx
+++ productive-app/src/components/ui/AddTaskButton.tsx
@@ -6,9 +6,9 @@ const NewTaskButton = () => {
     <button
       onClick={() => alert("New Task Button Clicked!")}
       className="
-        bg-[#2AB77C] text-white font-bold text-sm px-4 py-2 rounded-md
+        bg-[#2DC887] text-white font-bold text-sm px-4 py-2 rounded-md
         shadow-md 
-        hover:bg-[#24a46f] hover:shadow-lg 
+        hover:bg-[#26A671] hover:shadow-lg 
         transition-all duration-300
       "
     >
