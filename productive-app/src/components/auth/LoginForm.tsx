bf94e33a58f36e9d1a3e463c5817da8656aa3d8b
joe052 <joekaranjasenior52@gmail.com>
2025-11-20 13:19:15 +0300

    added formik to login form

--- productive-app/src/components/auth/LoginForm.tsx
+++ productive-app/src/components/auth/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";     
+import React from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import Link from "next/link";
@@ -49,12 +49,97 @@ const LogInForm: React.FC = () => {
           {/* SIGNIN FORM with Formik */}
           <Formik
             initialValues={{ email: "", password: "" }}
-            onSubmit={async () => {}}
+            validationSchema={validationSchema}
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
+              console.log("FORM SUBMITTED!!!");
+              console.log(values);
+            }}
           >
-            {() => (
-              <form>
-                <div className="">Hey</div>
-                <div className="">Hey</div>
+            {({
+              values,
+              errors,
+              touched,
+              isValid,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+            }) => (
+              <form className="space-y-6" onSubmit={handleSubmit}>
+                {/* EMAIL */}
+                <div className="">
+                  <label
+                    htmlFor=""
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Email
+                  </label>
+                  <div
+                    className={`rounded-md border ${
+                      errors.email && touched.email
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                  >
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange("email")}
+                      onBlur={handleBlur("email")}
+                      className="w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:rounded-md placeholder-gray-400"
+                      placeholder="Email"
+                    />
+                  </div>
+                  {/* ERROR BLOCK */}
+                  {errors.email && touched.email && (
+                    <p className="mt-1 text-xs text-red-500">{errors.email}</p>
+                  )}
+                </div>
+
+                {/* PASSWORD */}
+                <div className="">
+                  <label
+                    htmlFor=""
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+                  <div
+                    className={`rounded-md border ${
+                      errors.password && touched.password
+                        ? "border-red-500"
+                        : "border-gray-300"
+                    }`}
+                  >
+                    <input
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={values.password}
+                      onChange={handleChange("password")}
+                      onBlur={handleBlur("password")}
+                      className="w-full px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
+                      placeholder="Password"
+                    />
+                  </div>
+                  {/* ERROR BLOCK */}
+                  {errors.password && touched.password && (
+                    <p className="mt-1 text-xs text-red-500">
+                      {errors.password}
+                    </p>
+                  )}
+                </div>
+                {/* SUBMIT BUTTON */}
+                <div className="flex justify-center">
+                  <button
+                    type="submit"
+                    disabled={!isValid}
+                    className={`py-2 px-4 border border-transparent rounded-full shadow-md text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:bg-red-500 transition duration-150`}
+                  >
+                    Login
+                  </button>
+                </div>
               </form>
             )}
           </Formik>
