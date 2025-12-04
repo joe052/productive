e0e37c6bf685105791cbe8a6de4775531236d95b
joe052 <joekaranjasenior52@gmail.com>
2025-12-04 12:55:58 +0300

    modified auth - removed google auth

--- productive-app/src/components/auth/LoginForm.tsx
+++ productive-app/src/components/auth/LoginForm.tsx
@@ -6,7 +6,6 @@ import * as Yup from "yup";
 import Link from "next/link";
 import { createClient } from "@/lib/supabase/client";
 import { useRouter } from "next/navigation";
-import { signInWithGoogle } from "@/lib/action";
 
 /**COMPONENT */
 const LogInForm: React.FC = () => {
@@ -65,13 +64,13 @@ const LogInForm: React.FC = () => {
     }
   };
 
-  const handleGoogleSignIn = async () => {
-    try {
-      await signInWithGoogle();
-    } catch (error) {
-      setError(error instanceof Error ? error.message : "An error occurred");
-    }
-  };
+  // const handleGoogleSignIn = async () => {
+  //   try {
+  //     await signInWithGoogle();
+  //   } catch (error) {
+  //     setError(error instanceof Error ? error.message : "An error occurred");
+  //   }
+  // };
 
   /**TEMPLATE */
   return (
@@ -190,7 +189,7 @@ const LogInForm: React.FC = () => {
                   )}
                 </div>
                 {/* FORGOT PASSWORD */}
-                <div className="flex justify-end">
+                <div className="flex justify-begin">
                   <Link
                     href="/forgot-password"
                     className="text-sm text-green-500 hover:text-green-600 transition duration-150"
@@ -212,20 +211,19 @@ const LogInForm: React.FC = () => {
                 </div>
 
                 {/* OR */}
-                <div className="flex items-center justify-center mt-4 mb-4">
+                {/* <div className="flex items-center justify-center mt-4 mb-4">
                   <span className="border-b w-1/5 lg:w-1/4"></span>
                   <span className="text-xs text-gray-500 mx-2">OR</span>
                   <span className="border-b w-1/5 lg:w-1/4"></span>
-                </div>
+                </div> */}
 
                 {/* GOOGLE SIGN IN BUTTON */}
-                <div className="flex justify-center">
+                {/* <div className="flex justify-center">
                   <button
                     type="button"
                     onClick={handleGoogleSignIn}
                     className="w-full flex items-center justify-center py-2.5 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-green-100 hover:border-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150 cursor-pointer"
-                  >
-                    {/* Insert Google Icon SVG or Image here */}
+                  >                    
                     <img
                       className="w-4 h-4 mr-3"
                       src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
@@ -233,7 +231,7 @@ const LogInForm: React.FC = () => {
                     />
                     Sign in with Google
                   </button>
-                </div>
+                </div> */}
 
                 {/* SIGNUP LINK */}
                 <p className="mt-6 text-center text-sm text-gray-600">
@@ -254,51 +252,6 @@ const LogInForm: React.FC = () => {
           {error && (
             <p className="mt-4 text-center text-sm text-red-500">{error}</p>
           )}
-
-          {/* <form className="space-y-6">
-            <label
-              htmlFor=""
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
-              placeholder="Email"
-            />
-
-            <label
-              htmlFor=""
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
-              placeholder="Password"
-            />
-
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="py-2 px-4 border border-transparent rounded-full shadow-md text-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 transition duration-150"
-              >
-                Login
-              </button>
-            </div>
-            <p className="mt-6 text-center text-sm text-gray-600">
-              Don't have an account?{" "}
-              <Link
-                href="/signup"
-                className="font-medium text-green-500 hover:text-green-600 transition duration-150"
-              >
-                {" "}
-                Sign up
-              </Link>{" "}
-            </p>
-          </form> */}
         </div>
       </div>
     </div>
--- productive-app/src/lib/action.ts
+++ productive-app/src/lib/action.ts
@@ -1,34 +1,32 @@
-'use server'
+"use server";
 
-import { createClient } from '@/lib/supabase/server'
-import { redirect } from 'next/navigation'
-import { Provider } from '@supabase/supabase-js'
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { Provider } from "@supabase/supabase-js";
 
 const signInWith = (provider: Provider) => async () => {
-    const supabase = await createClient()
+  const supabase = await createClient();
 
-    // Ensure SITE_URL is defined, otherwise fallback to localhost or handle error
-    const auth_callback_url = `${process.env.SITE_URL}/callback`
+  // Ensure SITE_URL is defined, otherwise fallback to localhost or handle error
+  const auth_callback_url = `${process.env.SITE_URL}/callback`;
 
-    const { data, error } = await supabase.auth.signInWithOAuth({
-        provider,
-        options: {
-            redirectTo: auth_callback_url,
-        },
-    })
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo: auth_callback_url,
+    },
+  });
 
-    console.log({ data })
+  console.log({ data });
 
-    if (error) {
-        throw new Error(error.message)
-    }
+  if (error) {
+    throw new Error(error.message);
+  }
 
-    // TypeScript requires us to verify data.url is not null before passing it to redirect
-    if (data.url) {
-        redirect(data.url)
-    }
-}
+  // TypeScript requires us to verify data.url is not null before passing it to redirect
+  if (data.url) {
+    redirect(data.url);
+  }
+};
 
-const signInWithGoogle = signInWith('google')
-
-export { signInWithGoogle }
+const signInWithGoogle = signInWith("google");
